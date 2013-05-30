ba7f232ba753fc86aa08499eeeb4ea63b128936e
derwolfe <chriswwolfe@gmail.com>
2013-05-29 21:03:43 -0500

    add init pys

---
+++ test/server_tests.py
@@ -0,0 +1,10 @@
+from twisted.trial import unittest
+
+class BroadcastServerTests(unittest.TestCase):
+    def test_BroadcastServer(self):
+        pass
+
+
+class FileServerTests(unittest.TestCase):
+    def test_Fileserver(self):
+        pass
