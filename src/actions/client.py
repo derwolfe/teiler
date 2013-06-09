a385b1a3dee7569c590b37713e9b4fd6c00d7e33
derwolfe <chriswwolfe@gmail.com>
2013-06-09 15:50:51 -0500

    Add listener pieces

--- src/actions/client.py
+++ src/actions/client.py
@@ -3,15 +3,18 @@ from twisted.internet import reactor
 from twisted.application.internet import MulticastServer
 
 class MulticastClientUDP(DatagramProtocol):
+    
+    def startProtocol(self):
+        self.transport.joinGroup('228.0.0.5')
 
     def datagramReceived(self, datagram, address):
-            print "Received:" + repr(datagram)
+        print "Received: " + repr(datagram)
 
-# Send multicast on 224.0.0.1:8005, on our dynamically allocated port
-# the listener
 def main():
     print "Listening for an IP on 224.0.0.1:8005"
-    reactor.listenUDP(0, MulticastClientUDP()).write('UniqueID', ('224.0.0.1', 8005))
+    reactor.listenMulticast(8005, 
+                            MulticastClientUDP(),
+                            listen_multiple = True)
     reactor.run()
 
 if __name__ == '__main__':
