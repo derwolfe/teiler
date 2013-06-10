818e2e51b222425b6b05597c9255033abed01a9e
derwolfe <chriswwolfe@gmail.com>
2013-06-10 06:02:13 -0500

    change locals to local net

--- src/actions/locate.py
+++ src/actions/locate.py
@@ -4,14 +4,13 @@ import netifaces
 # The module is intended to be an abstraction that helps the user find the
 # local ip address that will be used for broadcasting. As windows, linux/mac 
 # handle their interfaces differently, we want the correct IP address found and used.
-# eth0 will be given precedence over broadcast(wifi).
 # """
 
 def get_live_interface():
     """will return a list of possible IPv4 addresses"""
     # basically try for wifi first, then ethernet
     addresses = []
-    locals = ['127.0.0.1', '127.0.1.1', '127.1.1.1']
+    local_network = ['127.0.0.1', '127.0.1.1', '127.1.1.1']
 
     # loop over the available network interfaces and try to get the LAN level IP
     for iface in netifaces.interfaces():
@@ -19,10 +18,10 @@ def get_live_interface():
 
         if test_iface is not None:
             for i in test_iface:
-                if i['addr'] not in locals:
+                if i['addr'] not in local_network:
                     addresses.append(i['addr'])
 
-    # return the address to to broadcast out
+    # return the address to broadcast out
     return addresses[0] 
 
                 
