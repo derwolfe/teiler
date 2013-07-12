2ff60a3de7ce1037e464eb7131ee1479dbdbb34e
derwolfe <chriswwolfe@gmail.com>
2013-07-11 21:23:30 -0500

    strip down the client for multicast only

--- src/actions/client.py
+++ src/actions/client.py
@@ -1,37 +1,14 @@
-import os
-import errno
-import sys
-
 from twisted.python import log
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 from twisted.application.internet import MulticastServer
 
-import requests
-
-_fileserver = ""
-_file_list = "teiler-list.txt"
-
-def get_file_urls(url):
-    full_url = "http://" + url + "/" + _file_list
-    r = requests.get(full_url)
-    if r.status_code == 200:
-        save(r.content, _file_list)
-        utils.make_files()
-    else:
-        print "Fileserver not at specified address"
-          
-def save(stuff, name):
-    with open(name, 'w') as f:
-        f.write(stuff)
-            
 class MulticastClientUDP(DatagramProtocol):
     
     def __init__(self):
         self.host = '224.0.0.5'
 
     def startProtocol(self):
-        # this could be placed in a config
         self.transport.joinGroup(self.host)
 
     def stopProtocol(self):
@@ -39,12 +16,8 @@ class MulticastClientUDP(DatagramProtocol):
 
     def datagramReceived(self, datagram, address):
         log.msg("Received: " + repr(datagram))
-        # switch to a local instance variable
-        global _fileserver
-        _fileserver = repr(datagram).replace("'", "")
-        # kill connection 
-        self.transport.loseConnection()
-        reactor.stop()    
+        
+
 
 def main():
     log.msg("Starting listener")
@@ -52,11 +25,6 @@ def main():
                             MulticastClientUDP(),
                             listenMultiple = True)
     reactor.run()
-    # async **should** be over
-    if _fileserver != "":
-        log.msg("Fileserver located at {0}".format(_fileserver))
-        get_file_urls(_fileserver)
-
 
 if __name__ == '__main__':
     main()
