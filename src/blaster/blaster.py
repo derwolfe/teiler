9197deffcdb92f7f74989844894029a81daa7db7
derwolfe <chriswwolfe@gmail.com>
2013-07-11 21:11:52 -0500

    initial commit for bidirectional communication

---
+++ src/blaster/blaster.py
@@ -0,0 +1,20 @@
+# blaster will be armin's take on how the program should be implemented.
+
+# Each instance of the application will be both a client and a server.
+
+# Instead a simple host that has all of the files to be shared. The application
+# will allow the user to send specific files to a specific user.
+
+# This means at least on the outset, we have to be able to find and store the location of the recipients. 
+
+# One user, will be able to send any of the other users a file. The recipient will approve the transfer. 
+
+# The workflow will be the following:
+
+# 1) two users; Bill, Shaq start the application.
+# 2) Bill has a document, shaq has a song. Bill wants the song, shaq wants the document.
+# 3) To get the the song, Bill finds shaq on his user list, and drops the file on his user name.
+
+#I don't like this
+
+
