3095dcfedcf7507f9c42e5445e6eab7959e1bcae
derwolfe <chriswwolfe@gmail.com>
2013-07-12 21:12:18 -0500

    wip for tests

--- src/actions/tests/test_server.py
+++ src/actions/tests/test_server.py
@@ -6,10 +6,9 @@ from zope.interface import implements
 from twisted.internet.interfaces import IMulticastTransport, IUDPTransport
 from twisted.trial import unittest
 
-# should these contain src?
 from .. import server
-from .. import utils 
-from .. import client
+
+
 
 class FakeUdpTransport(object):
     """ Instead of connecting through the network, this transport 
@@ -36,24 +35,22 @@ class FakeUdpTransport(object):
 class BroadcastServerTests(unittest.TestCase):
 
     def setUp(self):
-        self.protocol = server.Broadcaster('hi')
+        self.protocol = server.Broadcaster()
         self.tr = FakeUdpTransport()
         self.protocol.transport = self.tr
         
+        
     def test_broadcast(self):
-        self.protocol.sendHeartbeat()
+        self.protocol.sendMessage('hi')
         self.assertTrue(len(self.tr.msgs) > 0)
         self.assertTrue(self.tr.msgs[0] == "'hi'")
 
-class ClientTests(unittest.TestCase):
-
-    def test_kills_reactor(self):
-        self.assertTrue(False)
-
-    def test_receives_datagram(self):
-        self.assertTrue(False)
-
-
+    def test_datagram_received(self):
+        self.protocol.sendMessage("kowabunga")
+        # you are testing what happens when a msg is REC by the protocol.
+        # meaning you have to SEND the protocol to the client.
+        # i.e. self.tr.send...
+        
 
 
         
