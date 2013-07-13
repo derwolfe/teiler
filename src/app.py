0eca6005c7132d66d72b32d5bcfc4ce2cf7852ea
derwolfe <chriswwolfe@gmail.com>
2013-07-12 21:22:50 -0500

    remove client

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import sys
-from actions import server, client
+from actions import server
 from twisted.python import log
 
 # the main entry point for the application
@@ -16,21 +16,12 @@ _app_directory = os.path.join(os.path.expanduser("~"), "blaster")
 def main():
     log.startLogging(sys.stdout)
     parser = argparse.ArgumentParser(description="Exchange files!")
-    parser.add_argument('action',
-                        help="To be the server, type serve; to be the client, type listen",
-                        )
     args = parser.parse_args()
-    _app_runner(args.action)
 
-def _app_runner(how):
-    if how == "serve":
-        if os.path.exists(_app_directory) == False:
+def _app_runner():
+    if os.path.exists(_app_directory) == False:
             os.mkdir(_app_directory)
-        server.main(_app_directory)
-    elif how == "listen":
-        client.main()
-    else:
-        return u'Please specify either listen or serve'
+    server.main()
     
 
 if __name__  == '__main__':
