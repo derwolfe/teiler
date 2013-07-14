0aa271114c18c102e79a99690e265eadaa969527
Armin Graf <arminhammer@gmail.com>
2013-07-14 01:57:32 -0400

    Added initial file transfer functionality.  Not complete.

--- src/teiler.py
+++ src/teiler.py
@@ -5,6 +5,7 @@ import utils
 from actions import server
 from twisted.python import log
 from twisted.internet import reactor
+from filetransfer import FileReceiverFactory
 
 from peerdiscovery import PeerDiscovery
     
@@ -24,16 +25,25 @@ class Teiler:
         self.messages = []
         self.multiCastAddress = '230.0.0.30'
         self.multiCastPort = 8005
+        self.tcpPort = 9988
         
 def main():
     log.startLogging(sys.stdout)
     parser = argparse.ArgumentParser(description="Exchange files!")
     args = parser.parse_args()
+    
+    # Initialize peer discovery using UDP multicast
     multiCastPort = 8006
     teiler = Teiler()
     teiler.multiCastPort = multiCastPort
     log.msg("Initiating Peer Discovery")
-    reactor.listenMulticast(multiCastPort, PeerDiscovery(teiler), listenMultiple=True) 
+    reactor.listenMulticast(multiCastPort, PeerDiscovery(teiler), listenMultiple=True)
+    
+    #Initialize file transfer service
+    fileReceiver = FileReceiverFactory({})
+    reactor.listenTCP(teiler.tcpPort, fileReceiver)
+    log.msg("Starting file listener on ", teiler.tcpPort)
+    
     reactor.run()
 
 def _app_runner():
