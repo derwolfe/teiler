9a018794a4b64aa1ac8234ff14b0a838bc51f6f9
Chris Wolfe <chriswwolfe@gmail.com>
2013-09-04 17:37:05 -0500

    working on the line received tests
    Adding some tests for line receiveer at the moment and trying to figure out
    how all of this actually works.
    
    Also, replacing the print statements with logs

--- src/tests/test_filesending.py
+++ src/tests/test_filesending.py
@@ -1,4 +1,29 @@
-"""
-this will use the string transport to test how the protocol is actually meant
-to work on its own.
-""" 
+# There needs to be a good set of tests validating how the file sender protocol 
+# works. They will be here
+
+# test for the filesender client
+
+from twisted.trial import unittest
+from twisted.test.proto_helpers import StringTransport
+import json
+
+from ..filetransfer import FileReceiverProtocol
+
+
+class FileSenderClientTests(unittest.TestCase):
+    
+    def setUp(self):
+        self.proto = FileReceiverProtocol(".")
+        self.transport = StringTransport()
+        self.proto.transport = self.transport
+
+    def test_line_received(self):
+        "line received needs valid json"
+        line = json.dumps({"file_size" : 0, 
+                           "original_file_path": "./crap.txt"
+                           })
+        self.proto.lineReceived(line)
+        self.proto.instruction = line 
+        # has raw mode been set?
+        self.assertTrue(self.proto.line_mode == 0)
+        # suppress some of the prints
