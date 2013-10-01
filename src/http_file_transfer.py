693688b85f3e50b52df041db4ac4d0f2eabcbdb3
derwolfe <chriswwolfe@gmail.com>
2013-09-30 20:54:11 -0500

    adding the send request

--- src/http_file_transfer.py
+++ src/http_file_transfer.py
@@ -5,18 +5,42 @@ from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.web.resource import Resource
 from twisted.internet import reactor
+from twisted.web.client import getPage
+
+from urllib import urlencode
+
 ## file server
 
 ## 1 have the ability to post a file
 ## 2 serve the file at the url
 
 # file uploads suck in twisted, downloads don't   
-# for each file, create add a file, it can be a directory
-
 # each drop event should just use a sessionId and a filename
 # i.e. add resource root.putChild(sessionId + filename, File(filename))
+# a '' client '' will grab urls that it wants
+# a '' server '' will host the files it wants to send, and send posts
 
- 
+
+def sendFile(address, session, filename):
+    """
+    This function expects a network address, a session id, and a  
+    filename. Using this it will post a request a user.
+
+    The data contained in this request, will be used to download
+    the file from the sender.
+    """
+    postdata = urllib.urlencode({
+            'address': address,
+            'session': session,
+            'filename': filename
+            })
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+    return getPage(address, 
+                    method='POST', 
+                    postdata=postdata,
+                    headers=headers
+                )
+
 if __name__ == '__main__':
     #root = Resource()
     #root.putChild('r1', File("/Users/chris/Code/rust"))
