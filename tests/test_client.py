a5863407683084f6f1da5f49e060f814561cca09
derwolfe <chriswwolfe@gmail.com>
2014-04-19 09:57:20 -0500

    Factor out Dummy resources from client tests
    The dummy site, request, and root resource all live in the new
    _dummyresource.py file. This they can more easily be used in tests.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,68 +1,7 @@
 from twisted.trial import unittest
-from twisted.web.test.test_web import DummyRequest
-from twisted.web import resource
-from twisted.internet.defer import succeed, Deferred
-from twisted.web import server, client
-from twisted.web.static import File
-from twisted.python import log
-from twisted.protocols.policies import WrappingFactory
-from twisted.python.compat import networkString, nativeString
-from twisted.internet import reactor
-
-from urlparse import urljoin
-from filecmp import cmp
-from os import remove, path, makedirs
-from shutil import rmtree
-
-from teiler.client import FileRequestResource
 from teiler import server as tserver
+from ._dummyresource import DummyRootResource, DummySite
 
-class SmartDummyRequest(DummyRequest):
-    def __init__(self, method, url, args=None, headers=None):
-        DummyRequest.__init__(self, url.split('/'))
-        self.method = method
-        self.headers.update(headers or {})
-
-        args = args or {}
-        for k, v in args.items():
-            self.addArg(k, v)
-
-    def value(self):
-        return "".join(self.written)
-
-
-class DummySite(server.Site):
-    def get(self, url, args=None, headers=None):
-        return self._request("GET", url, args, headers)
-
-    def post(self, url, args=None, headers=None):
-        return self._request("POST", url, args, headers)
-
-    def _request(self, method, url, args, headers):
-        request = SmartDummyRequest(method, url, args, headers)
-        resource = self.getResourceFor(request)
-        result = resource.render(request)
-        return self._resolveResult(request, result)
-
-    def _resolveResult(self, request, result):
-        if isinstance(result, str):
-            request.write(result)
-            request.finish()
-            return succeed(request)
-        elif result is server.NOT_DONE_YET:
-            if request.finished:
-                return succeed(request)
-            else:
-                return request.notifyFinish().addCallback(lambda _: request)
-        else:
-            raise ValueError("Unexpected return value: %r" % (result,))
-
-class DummyRootResource(resource.Resource):
-    def __init__(self, requests, downloadTo):
-        resource.Resource.__init__(self)
-        self.putChild('requests', FileRequestResource(requests, downloadTo)) 
-
-# actual test code
 class FileRequestResourceTests(unittest.TestCase):
 
     def setUp(self):
