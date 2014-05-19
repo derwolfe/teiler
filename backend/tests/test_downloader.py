a69ea2ea1f78026bdcb7c25ad5057082c84df830
derwolfe <chriswwolfe@gmail.com>
2014-05-18 19:37:27 -0500

    formatting

--- backend/tests/test_downloader.py
+++ backend/tests/test_downloader.py
@@ -12,20 +12,21 @@ class FakeIoHandler(object):
 
     def open(self, filename):
         return
-    
+
     def close(self):
         return
 
     def write(self, bytes):
         self.buffer.append(bytes)
-    
+
+
 class FileWriterTests(unittest.TestCase):
-    
+
     def setUp(self):
         self.io = FakeIoHandler()
         self.progress = DownloadProgress()
-        self.protocol = FileWriter(Deferred(), 100, "plop", 
-                            self.io, self.progress)
+        self.protocol = FileWriter(Deferred(), 100, "plop",
+                                   self.io, self.progress)
 
     def test_receiving_data_writes_data_to_a_bufferlike_object(self):
         self.protocol.dataReceived(b'I have been written')
@@ -35,19 +36,20 @@ class FileWriterTests(unittest.TestCase):
         self.protocol.dataReceived(b'this was 17 bytes')
         self.assertTrue(self.protocol.currentProgress() == 17.0)
 
+
 class FileWriterProgressTests(unittest.TestCase):
-    
+
     def test_internal_progress_accessible(self):
         self.io = FakeIoHandler()
-        self.progress = DownloadProgress() 
-        self.protocol = FileWriter(Deferred(), 100, "plop", 
+        self.progress = DownloadProgress()
+        self.protocol = FileWriter(Deferred(), 100, "plop",
                                    self.io, self.progress)
         self.protocol.dataReceived(b'zehn zehn ')
         self.assertTrue(self.progress.current() == 10)
 
 
 class DownloadProgressTests(unittest.TestCase):
-    
+
     def test_can_update_progress(self):
         progress = DownloadProgress()
         progress.add(10.0)
