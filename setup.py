f17093f10eb5b1cfc31bb65857237af5b24993d1
derwolfe <chriswwolfe@gmail.com>
2014-08-13 08:15:17 -0500

    Close issue #10 - use extras_require instead of requirements-dev
    Switch to using setuptools' extras_require functionality to manage optional
    requirements. This way, setup.py is the sole manager of files to be installed.

--- setup.py
+++ setup.py
@@ -1,32 +1,28 @@
 import os, sys
 from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    here = os.path.abspath(".")
-    README = open(os.path.join(here, 'README.rst')).read()
-
-    install_requires = ["twisted", "netifaces"]
-
-    setup(
-        name="teiler",
-        description="simple LAN filesharing",
-        long_description=README,
-        version='0.1',
-        maintainer="Chris Wolfe",
-        maintainer_email="chriswwolfe@gmail.com",
-        packages=find_packages(),
-        include_package_data=True,
-        zip_safe=False,
-        license="MIT",
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Environment :: Web Environment",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python",
-        ] + [
+setup(
+    name="teiler",
+    description="simple LAN filesharing",
+    long_description= open(os.path.join('.', 'README.rst')).read(),
+    version='0.1',
+    maintainer="Chris Wolfe",
+    maintainer_email="chriswwolfe@gmail.com",
+    packages=['teiler'],
+    license="MIT",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ] + [
         ("Programming Language :: Python :: %s" % x) for x in
-            "2.7".split()],
-        install_requires=install_requires,
-        test_suite='tests'
+        "2.7".split()],
+    install_requires = ["twisted", "netifaces"],
+    extras_require = dict(
+        dev=["coverage", "pyflakes", "pep8", "flake8"]
+    ),
+    test_suite='tests'
 )
